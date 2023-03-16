28f016c216b95705de41c91b9a50806dfbee6d2a
augustinejoseph <augustine.augustine4@gmail.com>
2023-03-16 10:59:11 +0530

    fixed key error

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-c7_a=2^i6)o@clg2co#89&(#(5+a^j27z4(@!7=#wre_h&_qdo'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
